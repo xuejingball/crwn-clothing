f4886e2a821bef22520f0351c261d673f5b30ace
Jing_Xue <jxue@jxue.corp.beyond.ai>
2020-11-10 13:43:54 -0800

    added addItem and removeItem functionality to checkout item componenet

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,5 +1,5 @@
 import CartActionTypes from './cart.types';
-import {addItemToCart} from './cart.utils';
+import { addItemToCart, removeItemFromCart } from './cart.utils';
 
 const INITIAL_STATE = {
     hidden: true,
@@ -7,17 +7,29 @@ const INITIAL_STATE = {
 };
 
 const cartReducer = (state = INITIAL_STATE, action) => {
-    switch(action.type) {
+    switch (action.type) {
         case CartActionTypes.TOGGLE_CART_HIDDEN:
             return {
                 ...state,
                 hidden: !state.hidden
-            }
+            };
         case CartActionTypes.ADD_ITEMS:
             return {
                 ...state,
-                cartItems: addItemToCart([...state.cartItems], action.payload)
-            }
+                cartItems: addItemToCart(state.cartItems, action.payload)
+            };
+        case CartActionTypes.REMOVE_ITEMS:
+            return {
+                ...state,
+                cartItems: removeItemFromCart(state.cartItems, action.payload)
+            };
+        case CartActionTypes.CLEAR_ITEM_FROM_CART:
+            return {
+                ...state,
+                cartItems: state.cartItems.filter(
+                    cartItem => cartItem.id !== action.payload.id
+                )
+            };
         default:
             return state;
     }
