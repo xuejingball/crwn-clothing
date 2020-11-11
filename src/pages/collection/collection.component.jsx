2c39dfd072245d1482bf3a86ae8a54418ac02d7b
Jing_Xue <jxue@jxue.corp.beyond.ai>
2020-11-10 16:01:55 -0800

    nested routing for each collection

---
+++ src/pages/collection/collection.component.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {connect} from 'react-redux';
+
+import {selectCollection} from '../../redux/shop/shop.selectors';
+
+import CollectionItem from '../../components/collection-item/collection-item.component';
+
+import './collection.style.scss';
+
+const CollectionPage = ({ collection }) => (
+    <div className='collection-page'>
+        {
+            collection.items.map(item => (
+                <CollectionItem key={item.id} item={item}/>
+            ))
+        }
+    </div>
+)
+
+const mapStateToProps = (state, ownProps) => ({
+    collection: selectCollection(ownProps.match.params.collectionId)(state)
+})
+
+export default connect(mapStateToProps)(CollectionPage);
